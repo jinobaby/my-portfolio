782fa9947707a08e976eaf82f0fd7e8ff83cdc46
Jino Baby <jbaby2527@conestogac.on.ca>
2025-06-15 14:33:50 -0400

    navbar part and cursor splash effect added

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function About() {
+  return (
+    <div className='main-section'>
+        About
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Hoome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Hoome() {
+    return (
+        <div className='main-section'>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Hoome
